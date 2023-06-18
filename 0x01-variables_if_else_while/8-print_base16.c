6b6182b98724ff709ec946335249647f741bdafe
Abuuh <abrahamkipsang009@gmail.com>
2023-06-18 15:46:05 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char digit;
+	char digit;
 
-for (digit = '0'; digit <= '9'; digit++)
-{
-putchar(digit);
-}
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
 
-for (digit = 'a'; digit <= 'f'; digit++)
-{
-putchar(digit);
-}
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
 
-putchar('\n');
+	putchar('\n');
 
-return 0;
+	return (0);
 }
