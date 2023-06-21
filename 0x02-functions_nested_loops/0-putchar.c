4847f2afecc3514346d722aa8ad221274bb41414
Abuuh <abrahamkipsang009@gmail.com>
2023-06-20 17:50:47 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,33 @@
-#include *main.h*
+#include <unistd.h>
+
 /**
- * main - Entry poiny
- * Return: Always
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_')
+char c = '_';
+
+write(1, &c, 1);
+c = 'p';
+write(1, &c, 1);
+c = 'u';
+write(1, &c, 1);
+c = 't';
+write(1, &c, 1);
+c = 'c';
+write(1, &c, 1);
+c = 'h';
+write(1, &c, 1);
+c = 'a';
+write(1, &c, 1);
+c = 'r';
+write(1, &c, 1);
+c = '\n';
+write(1, &c, 1);
+
 return (0);
 }
