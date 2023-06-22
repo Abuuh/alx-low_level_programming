767755903475df2fe79e1e24ba22df855db2175d
Abuuh <abrahamkipsang009@gmail.com>
2023-06-22 15:32:36 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_most_numbers(void)
+{
+int number;
+for (number = 0; number <= 9; number++)
+{
+if (number != 2 && number != 4)
+{
+_putchar(number + '0');
+}
+}
+_putchar('\n');
+}
