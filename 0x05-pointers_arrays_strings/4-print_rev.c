f7fa2dc2a77b6ec7257c6f948a148a85b28449ad
Abuuh <abrahamkipsang009@gmail.com>
2023-06-26 04:16:38 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,39 +1,22 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to be written.
- *
- * Return: On success, returns the character written.
- * On error, returns -1 and sets errno.
- *
- * Description: This function writes the character 'c' to the
- * standard output (stdout).
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * print_rev - Prints a string in reverse.
- * @s: Pointer to the string to be printed.
- *
- * Description: This function prints the characters of the string
- * pointed to by 's' in reverse order, followed by a new line character ('\n').
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: The string to be printed in reverse.
  */
 void print_rev(char *s)
 {
-int length = 0;
+int len = 0;
 int i;
 
-/* Calculate the length of the string */
-while (s[length] != '\0')
-length++;
+while (s[len] != '\0')
+{
+len++;
+}
 
-/* Print the characters in reverse order */
-for (i = length - 1; i >= 0; i--)
+for (i = len - 1; i >= 0; i--)
+{
 _putchar(s[i]);
-
+}
 _putchar('\n');
 }
