0e6458332a1c5453409d6873717fb2c04b714089
Abuuh <abrahamkipsang009@gmail.com>
2023-06-26 15:20:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,29 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * main Generates random valid passwords for the
+ * program 101-crackme.
+ * Return: Always 0.
  */
 int main(void)
 {
-int passwordLength = 12; /* Adjust the length of the password as desired */
-char password[13]; /* Leave space for the null terminator */
-
-srand(time(0)); /* Seed the random number generator with current time */
-
-/* Generate random characters for the password */
-for (int i = 0; i < passwordLength; i++)
+char password [84];
+int index = 0, sum = 0, diff_half1, diff_half2;
+srand(time());
+while (sum < 2772)
 {
-/* Generate a random ASCII character within the desired range */
-password[i] = rand() % 94 + 33;
+password[index] = 33+ rand() % 94;
+sum += password [index++];
 }
-
-password[passwordLength] = '\0'; /* Add the null terminator at the end */
-
-printf("%s\n", password); /* Print the generated password */
-
+password[index] = '\0';
+if (sum != 2772)
+}
+diff_halfi diff half2
+(sum - 2772) / 2;
+(sum - 2772)/2;
+TUCERT
+if ((sum - 2772) % 2 != 0)
+diff_half1++;
+for (index = ; password[index]; index++)
+{
+if (password [index] >= (33+ diff_half1))
+{
+password[index] -= diff_half1;
+break;
+}
+}
+for (index = 0; password [index]; index++)
+{
+if (password[index] >= (33 + diff_half2))
+password[index] == diff_half2;
+break;
+}
+}
+}
+printf("", password);
 return (0);
 }
