d47e2f9d4cbe1af2db858342ebeba73911eb6cb2
Abuuh <abrahamkipsang009@gmail.com>
2023-06-26 03:51:04 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,26 +1,8 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to be written.
- *
- * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * _puts - Prints a string followed by a new line.
- * @str: Pointer to the string to be printed.
- *
- * Description: This function prints the characters of the string
- * pointed to by `str`, followed by a new line character ('\n').
- * It uses the `_putchar` function to write the characters to the
- * standard output.
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: The string to be printed.
  */
 void _puts(char *str)
 {
@@ -31,6 +13,5 @@ while (str[i] != '\0')
 _putchar(str[i]);
 i++;
 }
-
 _putchar('\n');
 }
