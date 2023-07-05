033a998d05ef84e596f6f67f65e7661724498fb0
Abuuh <abrahamkipsang009@gmail.com>
2023-07-05 08:04:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-
-/**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, the character written. On error, -1 is returned.
- */
-int _putchar(char c)
-{
-return (putchar(c));
-}
 
 /**
  * _puts_recursion - Prints a string followed by a new line using recursion
