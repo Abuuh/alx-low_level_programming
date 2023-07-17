496c00f454a750affcb9f83e463517e90747e7ad
Abuuh <abrahamkipsang009@gmail.com>
2023-07-17 07:09:06 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,18 @@
 #include "dog.h"
-#include <stddef.h>
 
 /**
- * init_dog - Initializes a struct dog with the provided values.
- * @d: Pointer to the struct dog to initialize.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
+ * init_dog - Initializes a struct dog
+ * @d: Pointer to the struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function initializes the members of struct dog.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
-{
+if (d != ((void *)0))
+{    
 d->name = name;
 d->age = age;
 d->owner = owner;
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* MAIN_H */
